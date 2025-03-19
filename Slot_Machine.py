098936ebde9019ec6d7a0575f2e14e0d031ac998
SudarshanAg31 <sudarshanagrawal65@gmail.com>
2025-03-19 11:23:26 +0530

    compelet

--- Slot_Machine.py
+++ Slot_Machine.py
@@ -14,11 +14,19 @@ def balance(current_blance,amount):
     current_blance=current_blance-amount
     return current_blance
 
-def check_result(z):
+def check_result(z,amount):
     if z[0]==z[1]==z[2]:
-        return "win"
-    else:
-        return "loose"
+        if z[0]=="L":
+            return amount*200
+        if z[0]=="K":
+            return amount*300
+        if z[0]=="J":
+            return amount*400
+        if z[0]=="H":
+            return amount*500
+        if z[0]=="G":
+            return amount*600
+    return 0
 
 current_blance=100    
 while True:
@@ -30,16 +38,18 @@ while True:
     amount=int(input("place your bet amount: "))
     z=choice()
     print(z)
-    a=check_result(z)
-    print(a)
+    a=check_result(z,amount)
+    if a==0:
+        print("loose")
+    else:
+        print("win")
+        current_blance=a+current_blance
+        
     current_blance=balance(current_blance,amount)
     print(f"your balance:{current_blance}")
     y=input("do you want to spin again?(Y/N):")
-    if y=="N"or"n":
+    if y==(("N")or("n")):
         print("thank you")
         break 
     else:
         pass
-        
-
-
